6e10b31ea9cafae44ea9b60ef1db939a0100e5b4
kaiyu.hu@groupe-esigelec.org <kaiyu.hu@groupe-esigelec.org>
2021-04-06 16:13:15 +0200

    AfterMerge

--- Client/obj/Debug/net5.0-windows/ExhibitPage.g.i.cs
+++ Client/obj/Debug/net5.0-windows/ExhibitPage.g.i.cs
@@ -53,7 +53,7 @@ namespace Client {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Client;component/exhibitpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Client;V1.0.0.0;component/exhibitpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ExhibitPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Client/obj/Debug/net5.0-windows/HallsPage.g.i.cs
+++ Client/obj/Debug/net5.0-windows/HallsPage.g.i.cs
@@ -53,7 +53,7 @@ namespace Client {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Client;component/hallspage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Client;V1.0.0.0;component/hallspage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\HallsPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
